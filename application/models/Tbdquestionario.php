13ed7ce99bea007321fc54e7051d395cd33fa172
Igor Pegoraro Armelin <igor_armelin@hotmail.com>
2019-10-10 00:04:35 -0300

    Update Tbdquestionario.php

--- application/models/Tbdquestionario.php
+++ application/models/Tbdquestionario.php
@@ -3,19 +3,7 @@ class Tbdquestionario extends CI_Model
 {
     public function buscaImagens()
     {
-        $categoria = $this->input->post('categorias');
-        $limite_fotos = $this->input->post('qtdFotos');
         
-        $query = $this->db->select('img.*')
-                            ->from('tbdimagem as img')
-                            ->join('imagem_categoria', 'img.idImagem = imagem_categoria.idImagem', 'inner')
-                            ->where("img.idImagem", $categoria);
-                            ->group_by('img.idImagem')
-                            ->order_by('rand()')
-                            ->limit($limite_fotos)
-                            ->get();
-        
-        return $query;
     }
         
 }
