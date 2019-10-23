9338c08d1b7a9d940f9f0eae7fd3088f9dfecf8b
Igor P. Armelin <igor_armelin@hotmail.com>
2019-10-23 12:37:32 -0300

    Correção classe display

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -1,5 +1,5 @@
             <section>
-                <h1 class="text-center display-3">Bem-vindo ao painel administrativo!</h1>
+                <h1 class="text-center display-4">Bem-vindo ao painel administrativo!</h1>
                 <p class="lead">Nesta área será possível fazer a inserção de novas imagens, cadastrar novas categorias e subcategorias para as imagens, e poderá visualizar os alunos que estão cadastrados no sistema.</p>
                 <p>Ao lado esquerdo, há o menu principal, onde você poderá navegar por todas as opções comentadas acima.</p>
             </section>
--- application/views/index.php
+++ application/views/index.php
@@ -1,5 +1,5 @@
             <section>
-                <h1 class="text-center display-3"> Bem-vindo(a) ao painel do aluno!</h1>
+                <h1 class="text-center display-4"> Bem-vindo(a) ao painel do aluno!</h1>
                 <p class="lead">Nesta área, será possível estudar fotos anatômicas, e também, realizar questionários sobre a categoria desejada. Ao final, será exibido o gabarito com a nota final sobre o questionário realizado.</p>
                 <p>Ao lado esquerdo, há o menu principal, onde você poderá navegar através das opções informadas acima.</p>
             </section>
