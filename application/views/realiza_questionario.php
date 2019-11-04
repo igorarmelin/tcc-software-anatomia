6b852b0e50da5e36199a51f3c6f2577d6a883988
Igor P. Armelin <igor_armelin@hotmail.com>
2019-11-04 17:34:07 -0300

    Correção v2.3 - Questionário

--- application/views/realiza_questionario.php
+++ application/views/realiza_questionario.php
@@ -44,5 +44,10 @@
     <script src="<?php echo base_url('assets/js/popper.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/bootstrap.min.js') ?>"></script>
     <script src="<?php echo base_url('assets/js/sidebar_function.js') ?>"></script>
+    <script>
+        $(function () {
+            $('[data-toggle="tooltip"]').tooltip()
+        })
+    </script>
     </body>
 </html>
